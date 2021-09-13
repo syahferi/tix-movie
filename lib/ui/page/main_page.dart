dbe3ddc32489dceeb9a7ac97dbc2a39de3b90a4d
syahferiaswan <syahferiaswan@gmail.com>
2021-09-13 16:43:04 +0700

    add custom bottom sheet with floating action bar

--- lib/ui/page/main_page.dart
+++ lib/ui/page/main_page.dart
@@ -9,28 +9,69 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Main Page'),
-        ),
-        body: BlocBuilder<UserBloc, UserState>(
-          builder: (context, state) {
-            if (state is UserLoadSuccess) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Hi ${state.user.name}'),
-                    ElevatedButton(
-                      child: Text('Sign Out'),
-                      onPressed: () async => await AuthService.signOut(),
-                    ),
-                  ],
+      body: Stack(
+        children: [
+          Container(color: accentColor1),
+          SafeArea(child: Container(color: Color(0xFFF6F7F9))),
+          ListView(),
+          createCustomNavBar(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: 46,
+              height: 46,
+              margin: EdgeInsets.only(bottom: 42),
+              child: FloatingActionButton(
+                elevation: 0,
+                backgroundColor: accentColor2,
+                child: SizedBox(
+                  height: 26,
+                  width: 26,
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.black.withOpacity(0.54),
+                  ),
                 ),
-              );
-            } else {
-              return SizedBox();
-            }
-          },
-        ));
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget createCustomNavBar() => Align(
+      alignment: Alignment.bottomCenter,
+      child: ClipPath(
+        clipper: BottomNavBarClipper(),
+        child: Container(
+          height: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+        ),
+      ));
+}
+
+class BottomNavBarClipper extends CustomClipper<Path> {
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(size.width / 2 - 28, 0);
+    path.quadraticBezierTo(size.width / 2 - 28, 33, size.width / 2, 33);
+    path.quadraticBezierTo(size.width / 2 + 28, 33, size.width / 2 + 28, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
   }
 }
