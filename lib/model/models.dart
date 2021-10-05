494c531ab91c77d77f225e358d140028f04a27e9
syahferiaswan <syahferiaswan@gmail.com>
2021-10-05 17:43:07 +0700

    add model movie, credit, and movie service

--- lib/model/models.dart
+++ lib/model/models.dart
@@ -1,7 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:image_picker/image_picker.dart';
 
+part 'credit.dart';
 part 'movie.dart';
+part 'movie_detail.dart';
 part 'promo.dart';
 part 'registration_data.dart';
 part 'user_tix.dart';
--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -16,4 +16,53 @@ class MovieService {
 
     return result.map((e) => Movie.fromJson(e)).toList();
   }
+
+  static Future<MovieDetail> getMovieDetails(Movie movie,
+      {http.Client? client}) async {
+    String url =
+        'https://api.themoviedb.org/3/movie/${movie.id}?api_key=$apiKey&languange=en-US';
+
+    client ??= http.Client();
+    var response = await client.get(Uri.parse(url));
+    var data = json.decode(response.body);
+
+    List genres = (data as Map<String, dynamic>)['genres'];
+    String language;
+
+    switch (data['original_language'].toString()) {
+      case 'ja':
+        language = 'Japanese';
+        break;
+      case 'id':
+        language = 'Indonesian';
+        break;
+      case 'ko':
+        language = 'Korean';
+        break;
+      case 'en':
+        language = 'English';
+        break;
+      default:
+        language = 'Unknown';
+    }
+
+    return MovieDetail(movie,
+        genres: genres.map((e) => e['title'].toString()).toList(),
+        language: language);
+  }
+
+  static Future<List<Credit>> getCredit(int movieId,
+      {http.Client? client}) async {
+    String url =
+        'https://api.themoviedb.org/3/movie/$movieId/credits?api_key=$apiKey';
+
+    client ??= http.Client();
+    var response = await client.get(Uri.parse(url));
+    var data = json.decode(response.body);
+
+    return (data['cast'] as List)
+        .map((e) => Credit(name: e['name'], profilePath: e['profile_path']))
+        .take(8)
+        .toList();
+  }
 }
